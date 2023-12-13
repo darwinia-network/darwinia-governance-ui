d0ad16de14cdf0a258850689b77c309a639ab8cb
JayJay1024 <lygm1062@gmail.com>
2023-12-13 18:24:39 +0800

    chore: util getChainLogoSrc

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from "./env";
 export * from "./chain";
+export * from "./misc";
---
+++ src/utils/misc.ts
@@ -0,0 +1,3 @@
+export function getChainLogoSrc(fileName: string) {
+  return `/images/chain/${fileName}`;
+}
